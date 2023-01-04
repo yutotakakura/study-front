77e573c10c42e634803a769aea4eae3a560d8961
Yuto Takakura <yutotakakura0320@gmail.com>
2023-01-04 20:49:45 +0900

    Reactの環境構築

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/react"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
